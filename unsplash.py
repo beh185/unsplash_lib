1c89350475fbb848fd6b7b4a56d898086c9d6d3b
Behnam <79264026+beh185@users.noreply.github.com>
2023-09-02 18:09:21 +0330

    Main File

--- unsplash.py
+++ unsplash.py
@@ -15,7 +15,7 @@ except ImportError:
 # =========== # Functions # =========== #
 
 # ======== # Downloading Images results for a query. # ======== #
-def search_photo(client_id = str(), query = str(), page = int(), per_page = int(), order_by = str(), collections = str(), content_filter = str(), color = str(), orientation = str(), _DownloadImg=True, _FileName: str = 'image', _Path = str()):
+def search_photo(client_id = str(), query = str(), page = int(), per_page = int(), order_by = str(), collections = str(), content_filter = str(), color = str(), orientation = str(), _DownloadImg=True, _FileName: str = 'S-image', _Path = str()):
     payload = {
         'client_id': client_id,
         'query': query,
@@ -33,22 +33,43 @@ def search_photo(client_id = str(), query = str(), page = int(), per_page = int(
     for key, value in payload.items():
         if (value == '' or value == 0):
             PayLoad.pop(key)
-
+    
+    # ======== # Checking requirement var # ======== #
+    if ('client_id' not in PayLoad.keys()):
+        exit('download_collection() missing 1 required positional argument. Please specify client_id')
+    if ('query' not in PayLoad.keys()):
+        exit('download_collection() missing 1 required positional argument. Please specify query')
+    
     unsplash_api = 'https://api.unsplash.com/search/photos'
     try:
         JsonImg = get(unsplash_api, allow_redirects=True, timeout=25, params=PayLoad)
-        if JsonImg.ok == False:
-            exit(f"[{JsonImg.status_code} Error] occurred while downloading images")
-        JsonImg = JsonImg.json()
     except ConnectionError:
         exit("Connection Failed. It's might because of your network connection")
     except KeyboardInterrupt:
         exit('\nOperation canceled by user')
     except ReadTimeout:
         exit('Time out reached')
-
+        
+# ======== # Checking for errors # ======== #
+    if(JsonImg.status_code != 200):
+        if(JsonImg.status_code == 400):
+            exit("[400 Error] The request was unacceptable, often due to missing a required parameter")
+        elif(JsonImg.status_code == 401):
+            exit("[401 Error] Invalid Access Token")
+        elif(JsonImg.status_code == 403):
+            exit("[403 Error] Missing permissions to perform request")
+        elif(JsonImg.status_code == 404):
+            exit("[404 Error] The requested resource doesn't exist")
+        elif(JsonImg.status_code == 500 or JsonImg.status_code == 503):
+            exit(f"[{JsonImg.status_code} Error] Something went wrong on unsplash's end")
+        else:
+            exit(f"[{JsonImg.status_code} Error] Something went wrong")
+    else:
+        JsonImg = JsonImg.json()
+        
+    # ======= # Download images# ======= #
     if(_DownloadImg):
-        # ======= # Download images# ======= #
+        print('Downloading images ...')
         if(_Path != str()):
             if(path.exists(_Path) == False):
                 exit(f'"{_Path}" is not exist')
@@ -66,13 +87,14 @@ def search_photo(client_id = str(), query = str(), page = int(), per_page = int(
                 exit('Error! It might be because of the network problem or your setting issues, such as proxy setting')
     else:
         return JsonImg
-# ======== # Get a single page of collection results for a query. # ======== #
-def search_collections(client_id = str ,query = str, page = int, per_page = int, _DownloadImg = True, _FileName=str(), _Path = str()):
+    
+# ========== # Retrieve a collection’s photos. # ========== #
+def download_collection(client_id = str(), id = str(), page = int, per_page = int, orientation = str(), _DownloadImg = True, _FileName = 'C-image', _Path = str()):
     payload = {
         'client_id': client_id,
-        'query': query,
         'page': page,
-        'per_page': per_page }
+        'per_page': per_page,
+        'orientation':orientation}
     # ======== # Making copy to prevent RuntimeError: dictionary changed size during iteration # ======== #
     PayLoad = payload.copy()
 
@@ -81,20 +103,42 @@ def search_collections(client_id = str ,query = str, page = int, per_page = int,
         if (value == '' or value == 0):
             PayLoad.pop(key)
 
-    unsplash_api = 'https://api.unsplash.com/search/collections'
+    # ======== # Checking requirement var # ======== #
+    if ('client_id' not in PayLoad.keys()):
+        exit('download_collection() missing 1 required positional argument. Please specify client_id')
+    if(id == str()):
+        exit('download_collection() missing 1 required positional argument. Please specify id')
+
+    unsplash_api = f'https://api.unsplash.com/collections/{str(id)}/photos'
     try:
         JsonImg = get(unsplash_api, allow_redirects=True, timeout=25, params=PayLoad)
-        if JsonImg.ok == False:
-            exit(f"[{JsonImg.status_code} Error] occurred while downloading images")
-        JsonImg.json()
     except ConnectionError:
         exit("Connection Failed. It's might because of your network connection")
     except KeyboardInterrupt:
         exit('\nOperation canceled by user')
     except ReadTimeout:
         exit('Time out reached')
+    
+    # ======== # Checking for errors # ======== #
+    if(JsonImg.status_code != 200):
+        if(JsonImg.status_code == 400):
+            exit("[400 Error] The request was unacceptable, often due to missing a required parameter")
+        elif(JsonImg.status_code == 401):
+            exit("[401 Error] Invalid Access Token")
+        elif(JsonImg.status_code == 403):
+            exit("[403 Error] Missing permissions to perform request")
+        elif(JsonImg.status_code == 404):
+            exit("[404 Error] The requested resource doesn't exist")
+        elif(JsonImg.status_code == 500 or JsonImg.status_code == 503):
+            exit(f"[{JsonImg.status_code} Error] Something went wrong on unsplash's end")
+        else:
+            exit(f"[{JsonImg.status_code} Error] Something went wrong")
+    else:
+        JsonImg = JsonImg.json()
+        
+    # ======= # Download images# ======= #
     if(_DownloadImg):
-        # ======= # Download images# ======= #
+        print('Downloading images ...')
         if(_Path != str()):
             if(path.exists(_Path) == False):
                 exit(f'"{_Path}" is not exist')
@@ -103,12 +147,66 @@ def search_collections(client_id = str ,query = str, page = int, per_page = int,
                 _Path = _Path + '\\'
             elif(name == 'posix' and _Path.endswith('/') == False):
                 _Path = _Path + '/'
-
         for i in range(per_page):
-            ImgLink = JsonImg['results'][i]['tags']['urls']['full']
+            ImgLink = JsonImg[i]['urls']['full']
             try:
                 urlretrieve(ImgLink, f"{_Path}{_FileName}-{i}.jpg")
             except URLError:
                 exit('Error! It might be because of the network problem or your setting issues, such as proxy setting')
+            except KeyboardInterrupt:
+                exit('\nOperation canceled by user')
     else:
         return JsonImg
+
+
+def get_collections_id(client_id = str(), query = str(), page = int(), per_page = int()):
+    payload = {
+        'client_id': client_id,
+        'query':query,
+        'page': page,
+        'per_page':per_page}
+    # ======== # Making copy to prevent RuntimeError: dictionary changed size during iteration # ======== #
+    PayLoad = payload.copy()
+
+    # ====== # Removing values that user didn't specified # ====== #
+    for key, value in payload.items():
+        if (value == '' or value == 0):
+            PayLoad.pop(key)
+    
+    # ======== # Checking requirement var # ======== #
+    if ('client_id' not in PayLoad.keys()):
+        exit('download_collection() missing 1 required positional argument. Please specify client_id')
+    if ('query' not in PayLoad.keys()):
+        exit('download_collection() missing 1 required positional argument. Please specify query')
+
+    unsplash_api = 'https://api.unsplash.com/search/collections'
+    try:
+        JsonData = get(unsplash_api, allow_redirects=True, timeout=25, params=PayLoad)
+    except ConnectionError:
+        exit("Connection Failed. It's might because of your network connection")
+    except KeyboardInterrupt:
+        exit('\nOperation canceled by user')
+    except ReadTimeout:
+        exit('Time out reached')
+    
+    # ======== # Checking for errors # ======== #
+    if(JsonImg.status_code != 200):
+        if(JsonImg.status_code == 400):
+            exit("[400 Error] The request was unacceptable, often due to missing a required parameter")
+        elif(JsonImg.status_code == 401):
+            exit("[401 Error] Invalid Access Token")
+        elif(JsonImg.status_code == 403):
+            exit("[403 Error] Missing permissions to perform request")
+        elif(JsonImg.status_code == 404):
+            exit("[404 Error] The requested resource doesn't exist")
+        elif(JsonImg.status_code == 500 or JsonImg.status_code == 503):
+            exit(f"[{JsonImg.status_code} Error] Something went wrong on unsplash's end")
+        else:
+            exit(f"[{JsonImg.status_code} Error] Something went wrong")
+    else:
+        JsonImg = JsonImg.json()
+    
+    id_list : list = list()
+    for i in range(per_page):
+        id_list.append(JsonData['results'][i]['id'])
+    return(id_list)
